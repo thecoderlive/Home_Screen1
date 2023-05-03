32d50b4ee382ac2673e2996de3e46f9361ea9e80
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-03 11:39:06 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,68 @@
+export const item = {
+    "id": "bb89945a-80e0-49bf-aa5a-a312cf0f4f3f",
+    "app_name": "debate",
+    "story": [
+        {
+            "id": "50a8af17-33dc-45f0-b5b2-e6e1c5774585",
+            "friends_story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png",
+            "name": "both"
+        },
+        {
+            "id": "6c5ce1f3-483a-4fdf-a405-29226d769ad0",
+            "friends_story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/368.png",
+            "name": "sister"
+        },
+        {
+            "id": "b8566340-d142-4230-98f8-162a7de82e9e",
+            "friends_story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png",
+            "name": "trip"
+        },
+        {
+            "id": "16e9fd7e-ef4c-493a-b3d2-94986a401020",
+            "friends_story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png",
+            "name": "soldier"
+        },
+        {
+            "id": "870b27b3-3b12-4d63-b21b-1cf42c5d3f41",
+            "friends_story": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png",
+            "name": "individual"
+        }
+    ],
+    "feed_list": [
+        {
+            "id": "0f413a0a-525c-4251-aa5f-a67b552a02ba",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png",
+            "content_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/68.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png",
+            "description": "Look any food. According yard sort song whole."
+        },
+        {
+            "id": "a25ae142-9232-42ae-940d-44a76cf45adb",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png",
+            "content_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/177.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png",
+            "description": "Actually position class PM worry company kind change. Put old radio television treat. Audience raise any even leader church director."
+        },
+        {
+            "id": "ba51bfd9-0a09-455c-80fe-db08a0a6742d",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png",
+            "content_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png",
+            "description": "Increase rest almost economic. Consumer practice dark but attorney rise successful. Traditional simple box lot help any response hear."
+        },
+        {
+            "id": "9c60d6b5-4a7f-435d-a0d3-38f4d3e2e322",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/25.png",
+            "content_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png",
+            "description": "Add pass pick write father speech physical. Glass sure somebody watch task drive machine. Around why her official develop."
+        },
+        {
+            "id": "c0698841-9020-4e01-8372-0153b6f87802",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png",
+            "content_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png",
+            "description": "Foot age boy choose realize into fly."
+        }
+    ]
+}
